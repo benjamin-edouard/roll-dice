0dc5bb1c4b9d38c7e820952c70989e0e93af6b9e
Benjamin ALLCOCK <benjamin.edouard@proton.me>
2023-07-07 21:49:48 +0200

    Coded the hold function

--- js/roll-dice.js
+++ js/roll-dice.js
@@ -1,6 +1,11 @@
 const newGame = document.querySelector('h1#new-game')
-const rollDice = document.querySelector('#role-dice')
+const rollDice = document.querySelector('#roll-dice')
 const holdScore = document.querySelector('#hold-score')
+
+// Defines the actual player
+const players = document.querySelectorAll('h2.actual-player')
+let actualPlayer = '0'
+
 // PLAYER ONE
 let playerOneGlobalScore = document.querySelector('div#player-one-global-score')
 let playerOneRoundScore = document.querySelector('span#player-one-round-score')
@@ -24,6 +29,14 @@ newGame.addEventListener('click', () => {
     }
 })
 
+// DEFINES THE ACTUAL PLAYER
+for (let i = 0; i < players.length; i++) {
+    players[i].addEventListener('click', () => {
+        actualPlayer = players[i].innerText.substring(7, 8)
+        players[i].style.fontWeight = 'bold'
+    })
+}
+
 // ROLL DICE
 rollDice.addEventListener('click', () => {
     alert('You have rolled the dice, good luck')
@@ -32,5 +45,13 @@ rollDice.addEventListener('click', () => {
 
 // HOLD SCORE
 holdScore.addEventListener('click', () => {
-    alert('You have hold your score')
+    if (actualPlayer === '1') {
+        playerOneGlobalScore.innerHTML = Number(playerOneRoundScore.innerText) + Number(playerOneGlobalScore.innerText)
+        playerOneRoundScore.innerHTML = 0
+    }
+
+    if (actualPlayer === '2') {
+        playerTwoGlobalScore.innerHTML = Number(playerTwoRoundScore.innerText) + Number(playerTwoGlobalScore.innerText)
+        playerTwoRoundScore.innerHTML = 0 
+    }
 })
