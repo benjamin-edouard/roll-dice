7e3ab2c9331682519ba43fed5c7f79583953888d
Benjamin ALLCOCK <benjamin.edouard@proton.me>
2023-07-09 21:41:39 +0200

    Completed the code for the roll dice function

--- js/roll-dice.js
+++ js/roll-dice.js
@@ -1,29 +1,26 @@
-const newGame = document.querySelector("h1#new-game")
-const rollDice = document.querySelector("#roll-dice")
-const holdScore = document.querySelector("#hold-score")
+// CONST actions necessary for the game
+const newGame = document.querySelector('h1#new-game')
+const rollDice = document.querySelector('#roll-dice')
+const holdScore = document.querySelector('#hold-score')
 const throwDiceArea = document.querySelector('#throw-dice-area')
 
 // Defines the actual player
-const players = document.querySelectorAll("h2.actual-player")
-let actualPlayer = "0"
+const players = document.querySelectorAll('h2.actual-player')
+let actualPlayer = '0'
 
 // PLAYER ONE
-let playerOneGlobalScore = document.querySelector(
-    "div#player-one-global-score"
-)
-let playerOneRoundScore = document.querySelector("span#player-one-round-score")
+let playerOneGlobalScore = document.querySelector('div#player-one-global-score')
+let playerOneRoundScore = document.querySelector('span#player-one-round-score')
 
 // PLAYER TWO
-let playerTwoGlobalScore = document.querySelector(
-    "div#player-two-global-score"
-)
-let playerTwoRoundScore = document.querySelector("span#player-two-round-score")
+let playerTwoGlobalScore = document.querySelector('div#player-two-global-score')
+let playerTwoRoundScore = document.querySelector('span#player-two-round-score')
 
 // LAUNCH NEW GAME
-newGame.addEventListener("click", () => {
+newGame.addEventListener('click', () => {
     // Player can confirm new game or not
     let confirmNewGame = confirm(
-        "Etes-vous sûr de vouloir commencer une nouvelle partie ?"
+        'Etes-vous sûr de vouloir commencer une nouvelle partie ?'
     )
 
     // If player confirms then the scores are set to 0
@@ -36,20 +33,34 @@ newGame.addEventListener("click", () => {
 })
 
 // DEFINES THE ACTUAL PLAYER -------------------------------------------------------------------
-players[0].addEventListener("click", () => {
+
+// This defines the actual player by clicking on the H1 element
+players[0].addEventListener('click', () => {
     actualPlayer = players[0].innerText.substring(7, 8)
-    players[0].classList.add("active-player")
-    players[1].classList.remove("active-player")
+    definePlayer(actualPlayer)
 })
 
-players[1].addEventListener("click", () => {
+players[1].addEventListener('click', () => {
     actualPlayer = players[1].innerText.substring(7, 8)
-    players[1].classList.add("active-player")
-    players[0].classList.remove("active-player")
+    definePlayer(actualPlayer)
 })
+
+// This function is called elsewhere in the program to define the player
+function definePlayer(player) {
+    if (player === '1') {
+        players[0].classList.add('active-player')
+        players[1].classList.remove('active-player')
+        actualPlayer = '1'
+    } else if (player === '2') {
+        players[1].classList.add('active-player')
+        players[0].classList.remove('active-player')
+        actualPlayer = '2'
+    }
+    return actualPlayer
+}
 
 // ROLL DICE -------------------------------------------------------------------
-rollDice.addEventListener("click", () => {
+rollDice.addEventListener('click', () => {
 
     let dice = new Map()
     dice.set(1, '<img src="../includes/images/dice/dice-one.png" class="dice-img" alt="dice one image">')
@@ -65,12 +76,23 @@ rollDice.addEventListener("click", () => {
             throwDice()
         } else {
             throwDiceArea.innerHTML = dice.get(diceNumber)
-            if(actualPlayer === '0') {
+
+            if (actualPlayer === '0') {
                 alert(`Veuillez sélectionner un joueur !`)
-            } else if(actualPlayer === '1') {
-                playerOneRoundScore.innerText = Number(playerOneRoundScore.innerText) + diceNumber
-            } else if(actualPlayer === '2') {
-                playerTwoRoundScore.innerText = Number(playerTwoRoundScore.innerText) + diceNumber
+            } else if (actualPlayer === '1') {
+                if (diceNumber != 1) {
+                    playerOneRoundScore.innerText = Number(playerOneRoundScore.innerText) + diceNumber
+                } else {
+                    playerOneRoundScore.innerText = 0
+                    definePlayer('2')
+                }
+            } else if (actualPlayer === '2') {
+                if (diceNumber != 1) {
+                    playerTwoRoundScore.innerText = Number(playerTwoRoundScore.innerText) + diceNumber
+                } else {
+                    playerTwoRoundScore.innerText = 0
+                    definePlayer('1')
+                }
             }
         }
     } 
@@ -78,14 +100,16 @@ rollDice.addEventListener("click", () => {
 })
 
 // HOLD SCORE -------------------------------------------------------------------
-holdScore.addEventListener("click", () => {
+holdScore.addEventListener('click', () => {
     if (actualPlayer === '0') {
         alert(`Veuillez sélectionner un joueur !`)
     } else if (actualPlayer === '1') {
         playerOneGlobalScore.innerHTML = Number(playerOneRoundScore.innerText) + Number(playerOneGlobalScore.innerText)
         playerOneRoundScore.innerHTML = 0
+        definePlayer('2')
     } else if (actualPlayer === '2') {
         playerTwoGlobalScore.innerHTML = Number(playerTwoRoundScore.innerText) + Number(playerTwoGlobalScore.innerText)
         playerTwoRoundScore.innerHTML = 0
+        definePlayer('1')
     }
 })
